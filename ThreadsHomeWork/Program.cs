f8d6b808ce168c461a29dbf5e6521a89e5902160
unknown <nelly-sherova@mail.ru>
2021-02-15 18:52:23 +0300

    Создала класс Client и  его свойства

--- ThreadsHomeWork/Program.cs
+++ ThreadsHomeWork/Program.cs
@@ -2,6 +2,15 @@
 
 namespace ThreadsHomeWork
 {
+
+    class Client
+    {
+        public int Id {set; get;}
+        public string Name {get; set;}
+        public int Age {get; set;}
+        public decimal Balance {get; set;}
+
+    }
     class Program
     {
         static void Main(string[] args)
